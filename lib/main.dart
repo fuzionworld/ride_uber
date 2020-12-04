d5089564163d048ba0ea519123dc17615c095ce3
Samuel Kow Paintsil <smashbros54@pop-os.localdomain>
2020-12-04 08:54:13 +0000

    🚧 home ui

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,39 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ride_uber/src/home/presentation/pages/home_page.dart';
 import 'package:ride_uber/src/login/pages/login_page.dart';
 import 'package:ride_uber/src/core/presentation/widgets/routes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final FirebaseApp app = await Firebase.initializeApp(
-      name: 'ride-uber',
-      options: Platform.isIOS
-          ? FirebaseOptions(
-              appId: '',
-              apiKey: 'AIzaSyAAaT-Vl8QDBk-JoxYQ9sG2GIF7Z5S9tm0',
-              projectId: 'ride-uber',
-              messagingSenderId: '213750194417',
-              databaseURL: 'https://ride-uber.firebaseio.com')
-          : FirebaseOptions(
-              appId: '1:213750194417:android:84eef140c230c224db0950',
-              apiKey: 'AIzaSyAAaT-Vl8QDBk-JoxYQ9sG2GIF7Z5S9tm0',
-              projectId: 'ride-uber',
-              messagingSenderId: '213750194417',
-              databaseURL: 'https://ride-uber.firebaseio.com'));
-  runApp(MaterialApp(
-    routes: Routes.routes,
-    home: LoginPage(),
-    title: 'Ride Uber',
-    theme: ThemeData(
-        fontFamily: 'Fira-Bold',
-        primarySwatch: Colors.lightGreen,
-        visualDensity: VisualDensity.adaptivePlatformDensity),
-  ));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  try {
+    final FirebaseApp app = await Firebase.initializeApp(
+        name: 'ride-uber',
+        options: Platform.isIOS
+            ? FirebaseOptions(
+                appId: '',
+                apiKey: 'AIzaSyAAaT-Vl8QDBk-JoxYQ9sG2GIF7Z5S9tm0',
+                projectId: 'ride-uber',
+                messagingSenderId: '213750194417',
+                databaseURL: 'https://ride-uber.firebaseio.com')
+            : FirebaseOptions(
+                appId: '1:213750194417:android:84eef140c230c224db0950',
+                apiKey: 'AIzaSyAAaT-Vl8QDBk-JoxYQ9sG2GIF7Z5S9tm0',
+                projectId: 'ride-uber',
+                messagingSenderId: '213750194417',
+                databaseURL: 'https://ride-uber.firebaseio.com'));
+  } catch (e) {} finally {
+    runApp(MaterialApp(
+      routes: Routes.routes,
+      home: HomePage(),
+      title: 'Ride Uber',
+      theme: ThemeData(
+          fontFamily: 'Fira-Bold',
+          primarySwatch: Colors.lightGreen,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+    ));
+  }
 }
